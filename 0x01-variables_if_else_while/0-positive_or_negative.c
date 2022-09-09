f8425e3c40b20c943714f514699605852647cf65
Tolutoon <getolopadetolu@gmail.com>
2022-09-09 16:57:57 -0700

    if else commits

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* main - main block
+ * if statements
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("The number is positive");
+        }
+	if (n = 0)
+       	{
+		printf("The number is zero");
+	}
+        if (n < 0)
+	{	
+		printf("The number is negative");
+	}
+	return 0;
+}
