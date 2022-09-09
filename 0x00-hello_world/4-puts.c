722c595498f03f9ba37c5d897995defcee657366
Tolutoon <getolopadetolu@gmail.com>
2022-09-09 00:05:53 -0700

    first c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - main block
  * return: 0
  */
-
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
