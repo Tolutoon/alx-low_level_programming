a360b8b1c4ac30e793f9795efb91417bc53d621c
Tolutoon <getolopadetolu@gmail.com>
2022-09-09 21:02:02 -0700

    my commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 /**
- * main - prints the lowercase alphabet in reverse,
+ * main - prints all the numbers of base 16 in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	int n;
 	char ch;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
 		putchar(ch);
 	}
