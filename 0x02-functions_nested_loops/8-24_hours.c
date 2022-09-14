007cf86660e20057eb483064e51adf3034c8b4d5
Tolutoon <getolopadetolu@gmail.com>
2022-09-14 06:55:29 -0700

    my commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
