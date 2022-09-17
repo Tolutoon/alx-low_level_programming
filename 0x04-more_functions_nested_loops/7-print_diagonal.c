d7dd41354f6dfd9a7aa82fe72e382d8d9d4ecfad
Tolutoon <getolopadetolu@gmail.com>
2022-09-16 20:30:30 -0700

    my commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,13 +19,11 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-				{
 					_putchar('\\');
 				else if (j < i)
 					_putchar(' ');
-				}
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
 	}
 }
