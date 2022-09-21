0a0cb5c7f331e68102c4e74affc48522c609c7b5
Tolutoon <getolopadetolu@gmail.com>
2022-09-21 11:57:18 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - fuction concatenates two strings
+ * @dest: pointer to destination character
+ * @src: pointer to source character
+ * Return: value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
