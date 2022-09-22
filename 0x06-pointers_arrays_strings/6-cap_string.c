d107b5228ae7225ea195b0e9beba68a837c9fb1d
Tolutoon <getolopadetolu@gmail.com>
2022-09-22 12:18:05 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * *cap_string - a function that capitalizes all words of a string.
+ * @s: pointer to the char
+ * Return: Capitalized strings.
+ */
+
+char *cap_string(char *s)
+{
+	char sep[] = ",\t;\n; .!?\"(){}";
+	int flag;
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 0;
+		if (i == 0)
+		{
+			flag = 1;
+		}
+		else
+		{
+			for (j = 0; sep[j] != '\0'; j++)
+			{
+				if (s[i - 1] == sep[j])
+					flag = 1;
+				break;
+			}
+		}
+		if (flag == 1)
+		{
+			if (s[i] <= 'z' && s[i] >= 'a')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s);
+}
+
