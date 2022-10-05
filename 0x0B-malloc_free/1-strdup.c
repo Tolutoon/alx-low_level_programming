ab17feee7b2922055198088c393584f8fea791fe
Tolutoon <getolopadetolu@gmail.com>
2022-10-05 15:59:25 -0700

    my commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ * @str: string passed to the function
+ *
+ * Return: pointer of an array of char
+ */
+
+char *_strdup(char *str)
+{
+	char *pstr;
+	int str_len, len, count;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	for (str_len = 0; str[str_len] != '\0'; str_len++)
+	{
+		len = str_len;
+	}	
+	pstr = malloc((len * sizeof(char)) + 1);
+
+	if (pstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		pstr[count] = str[count];
+	}
+	return (pstr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,6 @@
 #define _MAIN_H_
 
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 #endif
