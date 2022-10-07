d6e747ab33b291c1abfdc36db6b15149489e2792
Tolutoon <getolopadetolu@gmail.com>
2022-10-07 11:57:12 -0700

    my commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,5 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _putchar(char c);
-
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
