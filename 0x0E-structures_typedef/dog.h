d92ac6c53bcc79ac3d2f110bec2b136f09329799
Tolutoon <getolopadetolu@gmail.com>
2022-10-10 14:37:50 -0700

    my commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for dog characteristics
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
