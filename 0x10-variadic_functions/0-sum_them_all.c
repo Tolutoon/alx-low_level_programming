3b4ca1311df84006e07bafeda2e2df56c210407b
Tolutoon <getolopadetolu@gmail.com>
2022-10-13 08:28:57 -0700

    my commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
  * sum_them_all - a function that returns the sum of all its parameters.
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
 #endif 
