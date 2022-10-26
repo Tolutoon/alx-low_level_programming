c7134633b2576691ec56f808cedc20cdc7161111
Tolutoon <getolopadetolu@gmail.com>
2022-10-25 23:37:19 -0700

    betty issues fixed

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,10 +11,10 @@
  ** @next: points to the next node
  **
  ** Description: singly linked list node structure
- ** 
+ **
  */
 
- typedef struct listint_s
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
