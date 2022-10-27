761df2b08666109efe982986b866c8cd2b6bfca2
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-10-27 21:12:16 +0100

    betty errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,9 +9,9 @@
 
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-    {
-        print_binary(n >> 1);
-    }
-    _putchar((n & 1) + 48);
+	if (n > 1)
+	{
+	print_binary(n >> 1);
+	}
+	_putchar((n & 1) + 48);
 }
