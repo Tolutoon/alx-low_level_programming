85212b5cca2c1aaab7cd244dc39cd8220d584d32
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-10-27 20:05:34 +0100

    my commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,4 +2,28 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
- *  
+ * @b: pointer to strings of 0's and 1's char.
+ *
+ * Return: the converted number or 0 if
+ * char is not between 0 and 1 or if b is NULL.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int base_ten = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+		{
+			return (0);
+		}
+		base_ten = 2 * base_ten + (b[i] - '0');
+	}
+	return (base_ten);
+}
