fa0636ac51c85098aa9e7175843645e862064957
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-10-27 21:05:59 +0100

    task 1 documentation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary of a number.
+ * @n: input number o be converted
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+    if (n > 1)
+    {
+        print_binary(n >> 1);
+    }
+    _putchar((n & 1) + 48);
+}
