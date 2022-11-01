d06242fb26a29ea958347d8690dddbdb4f5e30d4
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-11-01 01:00:39 +0100

    Betty check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 	for (i = 0; text_content[i]; i++)
 		;
 	rw = write(fd, text_content, i);
